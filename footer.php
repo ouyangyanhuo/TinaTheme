3e3b19f622d592a171198e428e9dce1028e47b67
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-05-29 14:43:53 +0800

    fix articles' bug and add update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 </div>
 <script>
 $(document).pjax(
-  'a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"],a[no-pjax]), a[href^="?"]',
+  'a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"],a[no-pjax]), a[href^="?"], a[href^="/"]',
   {
     container: '#pjax-load',
     fragment: '#pjax-load',
