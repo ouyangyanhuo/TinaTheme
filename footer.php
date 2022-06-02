afdb672e9f8e8c2e35ad2cd6821eaee213e37278
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-06-02 21:44:15 +0800

    Build22602-No.6-FIXNo.5Problem

--- footer.php
+++ footer.php
@@ -1,11 +1,3 @@
-<?php if ($this->options->WebPjax): ?>
-  <script src="<?php $this->options->themeUrl('/assets/js/pjax.min.js'); ?>"></script>
-  <link href="https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.css" rel="stylesheet">
-  <script src="https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.js"></script>
-  </div>
-  <script>$(document).pjax('a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"],a[no-pjax]), a[href^="?"], a[href^="/"]',{container:'#pjax-load',fragment:'#pjax-load',timeout:8000}).on('pjax:send',function(){NProgress.start()}).on('pjax:complete',function(){NProgress.done()});</script>
-<?php endif; ?>
-</body>
 <footer class="footer flex">
     <section class="container">
         <nav class="footer-links">
@@ -19,6 +11,14 @@
     <script async="" src="<?php $this->options->themeUrl('/assets/js/features.js'); ?>" data-enable-footnotes="true"></script>
     <?php endif; ?>
 </footer>
+<?php if ($this->options->WebPjax): ?>
+  <script src="<?php $this->options->themeUrl('/assets/js/pjax.min.js'); ?>"></script>
+  <link href="https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.css" rel="stylesheet">
+  <script src="https://cdn.bootcdn.net/ajax/libs/nprogress/0.2.0/nprogress.min.js"></script>
+  </div>
+  <script>$(document).pjax('a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"],a[no-pjax]), a[href^="?"], a[href^="/"]',{container:'#pjax-load',fragment:'#pjax-load',timeout:8000}).on('pjax:send',function(){NProgress.start()}).on('pjax:complete',function(){NProgress.done()});</script>
+<?php endif; ?>
+</body>
 <?php if ($this->options->cursor): ?>
   <script async="" src="<?php $this->options->themeUrl('/assets/js/cursor.js'); ?>" data-enable-footnotes="true"></script>
 <?php endif; ?>
