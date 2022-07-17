14dceed0c9fc37ca39057bc8ddd80b76d108a64e
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-07-17 16:04:30 +0800

    1.2.8-Build22717-1-Dev

--- README.md
+++ README.md
@@ -1,10 +1,82 @@
+<!--
+ * @Author: ouyangyanhuo ouyangyanhuo@vip.qq.com
+ * @Date: 2022-05-28 18:42:41
+ * @LastEditors: ouyangyanhuo ouyangyanhuo@vip.qq.com
+ * @LastEditTime: 2022-07-17 15:47:49
+ * @FilePath: \SmileTheme\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 ## SmileTheme
 
-一个为Typecho移植的主题，源自于[hugo-tania](https://github.com/WingLim/hugo-tania)主题，原本是Hugo的主题，被移植到Typecho上在其基础上进行了深度修改的Smiletheme或许是你在Typecho上最好的选择
+一个为typecho移植的主题，源自于[hugo-tania](https://github.com/WingLim/hugo-tania)主题，原本是Hugo的主题，被移植到Typecho上在其基础上进行了深度修改的Smiletheme或许是你在Typecho上最好的选择
 
-## DEV 通道
+[![AdminMD](https://img.shields.io/badge/Magneto-SmileTheme-brightgreen?style=for-the-badge)](https://fmcf.cc/technology/523/)
+![Version](https://img.shields.io/badge/Version-1.2.7-critical?style=for-the-badge)
+![Repo-size](https://img.shields.io/github/languages/code-size/ouyangyanhuo/SmileTheme?style=for-the-badge)
+![License](https://img.shields.io/github/license/ouyangyanhuo/SmileTheme?style=for-the-badge)
+![Stars](https://img.shields.io/github/stars/ouyangyanhuo/SmileTheme?style=for-the-badge)
+![Forks](https://img.shields.io/github/forks/ouyangyanhuo/SmileTheme?style=for-the-badge&k)
 
-这是SmileTheme的Dev通道，是一个未完全开发完成的版本，仅供尝鲜，生产环境请勿轻易尝试使用。
+原主题：[https://github.com/WingLim/hugo-tania](https://github.com/WingLim/hugo-tania)
 
-## 目前问题
-- 暂未发现影响正常使用的Bug
+## 最近版本更新日志
+
+- 修复主题应用时存在的Bug
+
+## 使用提示
+
+- 在1.2.7版本更新时，添加了评论区头像优化，使得评论区头像可以通过QQ邮箱解析，并且更改了默认头像源，评论区头像优化是一项无法关闭的内容，如果需要关闭或修改 Gravatar 的代理地址，请前往 `/主题所在目录/core/functions.php` 第 190-211 行 进行修改。
+
+## 功能与特性
+
+- 简洁风格
+
+- 适配深色模式
+
+- HTML 原生自响应式布局
+
+- 超轻量
+
+- 高度自定义
+
+- 完善的后台设置系统
+
+欢迎提 Issues 和 PRs，欢迎提出建议
+
+## 使用
+下载主题包并解压 ( 若是从 GitHub 或 Gitee 下载，请把解压出来的文件夹改名为 `SmileTheme` ) ，将文件夹上传至网站文件主题目录 ( `/usr/theme` ) 下，进入网站后台 - 控制台 - 外观 - 启用主题即可。
+
+## 文档
+
+SmileTheme 文档 : [https://smiletheme.docs.fmcf.cc](https://smiletheme.docs.fmcf.cc)
+
+## Demo
+
+主题效果预览
+
+SmileTheme主题演示站：[https://smiletheme.fmcf.cc](https://smiletheme.fmcf.cc)
+
+## 注意
+
+SmileTheme 使用 [GPL V3.0](https://github.com/ouyangyanhuo/SmileTheme/blob/main/LICENSE) 协议开源，请遵守此协议进行二次开发等。
+
+您必须在页脚保留 SmileTheme 主题的名称及其指向链接，否则请不要使用 SmileTheme 主题。
+
+您可以删除页脚的作者版权信息，但是不能删除 SmileTheme 主题的名称及其指向链接。
+
+## 捐赠
+
+如果您很喜欢这个主题，不妨[捐赠](https://www.verypoor.cn)一下我，这样有助于我更高效的开发
+
+## 下载渠道
+
+1.GitHub [https://github.com/ouyangyanhuo/SmileTheme](https://github.com/ouyangyanhuo/SmileTheme)  适合国外（主仓库）
+
+2.取消 Gitee 下载 因为它真的比 Shit 还烂，Gitee仓库会保留，但不会同步、更新。
+
+## 预览图
+![](https://i.loli.net/2021/08/16/nmvW5uVKHChafsd.png)
+![](https://i.loli.net/2021/08/16/sARYjilgO9MF2H1.png)
+![](https://i.loli.net/2021/08/16/rcXL5KjbueZ6zQp.png)
+![](https://i.loli.net/2021/08/16/wLxGjiPm6pZoyBt.png)
+![](https://i.loli.net/2021/08/16/Cq1Dymtsur4eTAV.png)
--- post.php
+++ post.php
@@ -1,31 +1,35 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 <main>
-        <div class="container">
-            <article>
-                <header class="article-header">
-                    <div class="thumb">
-                        <div>
-                            <h1><?php $this->title() ?></h1>
-                            <div class="post-meta">
-                                <div>
-                                <a>By <a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a></a> | 
-                                <time><?php $this->date('M j, Y'); ?></time> |
-                                <a><?php _e('Category: '); ?><?php $this->category(','); ?></a>
-                                <?php if ($this->options->WordCount): ?>
+    <div class="container">
+        <article>
+            <header class="article-header">
+                <div class="thumb">
+                    <div>
+                        <h1><?php $this->title() ?></h1>
+                        <div class="post-meta">
+
+                            <div>
+                            <a>
+                                By <a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a>
+                            </a> |
+                            <a>
+                                <?php _e('Category: '); ?><?php $this->category(','); ?>
+                            </a>
+                            <?php if ($this->options->WordCount): ?>
                                 <time> | 共 <?php echo word_count($this->cid); ?> 个字符</time>
-                                <?php endif; ?>
-                                </div>
-                                <div class="tags">
-                                    <?php $this->tags(', ', true, '<a>NoTag</a>'); ?>
+                            <?php endif; ?>
                             </div>
+                            <div class="tags">
+                                <?php $this->tags(', ', true, '<a>NoTag</a>'); ?>
                         </div>
                     </div>
-                </header>
-            </article>
-            <div id="post" class="article-post">
+                </div>
+            </header>
+        </article>
+        <div id="post" class="article-post">
             <?php if ($this->options->fancybox): ?>
-            <?php
+                <?php
                 $pattern = '/\<img.*?src\=\"(.*?)\".*?alt\=\"(.*?)\".*?title\=\"(.*?)\"[^>]*>/i';
                 $replacement = '<a href="$1" data-fancybox="gallery" /><img src="$1" alt="$2" title="$3"></a>';
                 $content = preg_replace($pattern, $replacement, $this->content);
@@ -33,28 +37,29 @@
                 $array=explode('<!--more-->', $content);
                 $content=$array[0];
                 echo getContentTest($content)
-            ?>
+                ?>
             <?php else: ?>
-            <?php echo getContentTest($this->content); ?>
+                <?php echo getContentTest($this->content); ?>
             <?php endif; ?>
-            </div>
         </div>
-        <?php if ($this->options->TheComments): ?>
+    </div>
+    <?php if ($this->options->TheComments): ?>
         <div class="container">
             <?php $this->need('comments.php'); ?>
         </div>
-        <?php else: ?>
+    <?php else: ?>
         <div class="container">
             <div class="alert info">已关闭全局评论。</div>
         </div>
-        <?php endif; ?>
-        <div class="container">
-            <nav class="flex container suggested">
-                    上一篇
-                    <?php $this->thePrev('%s', '<a href="#">没有了</a>');?>
+    <?php endif; ?>
+    <div class="container">
+        <nav class="flex container suggested">
+                上一篇
+                <?php $this->thePrev('%s', '<a href="#">没有了</a>');?>
             
-                    下一篇
-                    <?php $this->theNext('%s', '<a href="#">没有了</a>');?>
-            </nav>
-        </div>
+                下一篇
+                <?php $this->theNext('%s', '<a href="#">没有了</a>');?>
+        </nav>
+    </div>
 <?php $this->need('footer.php'); ?>
+<!-- 完成重构 -->
