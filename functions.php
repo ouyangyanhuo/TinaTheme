9171266406d9409913ed12b975f6ac394a5bb3ba
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-07-30 11:57:41 +0800

    2.0.1-Build-Dev-No.1

--- functions.php
+++ functions.php
@@ -145,6 +145,17 @@ function themeConfig($form) {
         _t('评论区的加减法验证系统，关闭后反垃圾评论效果降低。')
     );
     $form->addInput($TheVerification);
+
+    $GravatarUrl = new Typecho_Widget_Helper_Form_Element_Radio('GravatarUrl', 
+    array
+    (
+        false => _t('官方源'),
+        'https://cdn.helingqi.com/avatar/' => _t('禾令奇源'),
+        'https://sdn.geekzu.org/avatar/' => _t('极客族源'),
+        'https://dn-qiniu-avatar.qbox.me/avatar/' => _t('七牛源')
+	),
+	'https://cdn.helingqi.com/avatar/', _t('Gravatar头像源'), _t('默认禾令奇源'));
+	$form->addInput($GravatarUrl);
     
      /* Notice */
     $TheNotice = new Typecho_Widget_Helper_Form_Element_Text('TheNotice', NULL, NULL, _t('<h2>网站公告 <small>Notice</small></h2>'));
