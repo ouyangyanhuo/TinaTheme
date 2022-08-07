67b6490242591efa0a7d378111d58a56a095f161
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-08-07 17:13:21 +0800

    Build22815-Dev-fix page-links.php more tag

--- page-links.php
+++ page-links.php
@@ -27,9 +27,6 @@
                 $pattern = '/\<img.*?src\=\"(.*?)\".*?alt\=\"(.*?)\".*?title\=\"(.*?)\"[^>]*>/i';
                 $replacement = '<a href="$1" data-fancybox="gallery" /><img src="$1" alt="$2" title="$3"></a>';
                 $content = preg_replace($pattern, $replacement, $this->content);
-                //内容截断
-                $array=explode('<!--more-->', $content);
-                $content=$array[0];
                 echo getContentTest($this->content);
             ?>
             <?php else: ?>
