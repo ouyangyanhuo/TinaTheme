765fc06d3cd7efdba72ce8aa8238b84a5cf8e599
ouyangyanhuo <ouyangyanhuo@vip.qq.com>
2022-08-31 13:58:20 +0800

    Build22101 Dev some change

--- comments.php
+++ comments.php
@@ -42,26 +42,25 @@ echo $commentClass;
 </div>
 <br>
 <?php } ?>
-
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div id="comments">
     <?php $this->comments()->to($comments); ?>
     <?php if($this->allow('comment')): ?>
-        <div id="<?php $this->respondId(); ?>" class="respond">
+    <div id="<?php $this->respondId(); ?>" class="respond">
     	<h4 class="article-post"><?php _e('添加新评论'); ?> <small><?php $comments->cancelReply(); ?></small></h4>
     	<form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" class="comment-form" role="form">
             <?php if($this->user->hasLogin()): ?>
     		<p><?php _e('登录身份: '); ?><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout" no-pjax><?php _e('退出'); ?> &raquo;</a></p>
             <?php else: ?>
-    		<p class="author-1">
+    		<p class="areatype-1">
                 <label for="author" class="required"><?php _e('称呼'); ?></label>
     			<input type="text" name="author" id="author" class="text" value="<?php $this->remember('author'); ?>" required />
     		</p>
-    		<p class="mail-2">
+    		<p class="areatype-1">
                 <label for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif; ?>><?php _e('Email'); ?></label>
     			<input type="text" name="mail" id="mail" class="text" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
     		</p>
-    		<p class="url-3">
+    		<p class="areatype-2">
                 <label for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif; ?>><?php _e('网站'); ?></label>
     			<input type="text" name="url" id="url" class="text" placeholder="<?php _e('http://'); ?>" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
     		</p>
